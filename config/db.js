129b0045c863521c6e808480f7af90aec2183792
Quality2024-max <apex5759@gmail.com>
2026-09-25 22:24:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -12,8 +12,8 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  ssl: {
-    ca: fs.readFileSync("./ca.pem"),
+    ssl: {
+    ca: process.env.DB_CA_CERT,
     rejectUnauthorized: true,
   },
 
